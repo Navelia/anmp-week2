e89bb414430d3b37d8552f9bb5c9897d1eac5f74
Navellia <ncent4lyfe@gmail.com>
2023-03-08 12:38:03 +0700

    test commit push

--- app/src/main/java/com/ncents/adv160420003week2/MainFragment.kt
+++ app/src/main/java/com/ncents/adv160420003week2/MainFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -27,6 +29,16 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnStart.setOnClickListener {
+            val playerName = txtName.text.toString()
+            val action = MainFragmentDirections.actionGameFragment(playerName)
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 
     companion object {
--- app/src/main/java/com/ncents/adv160420003week2/ResultFragment.kt
+++ app/src/main/java/com/ncents/adv160420003week2/ResultFragment.kt
@@ -7,11 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_game.*
+import kotlinx.android.synthetic.main.fragment_result.*
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class GameFragment : Fragment() {
+class ResultFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
@@ -28,54 +29,27 @@ class GameFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        return inflater.inflate(R.layout.fragment_result, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        if(arguments != null) {
-//            val playerName = GameFragmentArgs.fromBundle(requireArguments()).playerName
-//            txtTurn.text = "$playerName's Turn"
-//        }
-
-        var num1 = 0
-        var num2 = 0
-        var result = 0
-        var score = 0
-
-        arguments?.let{
-            val playerName = GameFragmentArgs.fromBundle(requireArguments()).playerName
-            txtTurn.text = "$playerName's Turn"
-
-            num1 = (0..100).shuffled().last()
-            num2 = (0..100).shuffled().last()
-            result = num1 + num2
-            txtQuestion.text = "$num1 + $num2"
+        arguments?.let {
+            val resultScore = ResultFragmentArgs.fromBundle(requireArguments()).resultScore
+            txtScoreResult.text = "Your score is $resultScore"
         }
 
-        btnSubmit.setOnClickListener {
-            var userAnswer = txtAnswer.text.toString().toInt()
-            if(userAnswer == result){
-                score += 1
-                num1 = (0..100).shuffled().last()
-                num2 = (0..100).shuffled().last()
-                result = num1 + num2
-                txtQuestion.text = "$num1 + $num2"
-                txtAnswer.setText("")
-            }
-            else{
-                val action = GameFragmentDirections.actionResultFragment(score)
-                Navigation.findNavController(it).navigate(action)
-            }
+        btnBackMain.setOnClickListener{
+            val action = ResultFragmentDirections.actionMainFragment()
+            Navigation.findNavController(it).navigate(action)
         }
-
     }
 
 //    companion object {
 //        @JvmStatic
 //        fun newInstance(param1: String, param2: String) =
-//            GameFragment().apply {
+//            ResultFragment().apply {
 //                arguments = Bundle().apply {
 //                    putString(ARG_PARAM1, param1)
 //                    putString(ARG_PARAM2, param2)
